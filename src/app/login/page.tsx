9e473f294d6e49414d12e703bd0e0c079bb3c98c
Sk Alijan <alizaan8695@gmail.com>
2023-12-15 18:24:13 +0530

    login and signup page created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,65 @@
-const Login = () => {
-  return <div></div>;
+"use client"
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+
+const SignIn: React.FC = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = async () => {
+    // Implement your sign-in logic here
+    // You can use the 'email' and 'password' states
+  };
+
+  const handleGoogleSignIn = async () => {
+
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white p-10 rounded shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Sign In</h2>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+          />
+        </div>
+        <button onClick={handleSignIn} className="bg-blue-500 text-white p-2 rounded-md mb-4 w-full">
+          Sign In
+        </button>
+        <button onClick={handleGoogleSignIn} className="bg-red-500 text-white p-2 rounded-md w-full">
+          Sign In with Google
+        </button>
+
+        <p className='mt-3'>New User ? <Link href={"./signup"} className='font-bold'>Signup here</Link> </p>
+      </div>
+    </div>
+  );
 };
 
-export default Login;
+export default SignIn;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,85 @@
-import React from "react";
 
-const Signup = () => {
-  return <div></div>;
+"use client"
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+
+const SignUp: React.FC = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    // Implement your sign-up logic here
+    // You can use the 'email' and 'password' states
+  };
+
+  const handleGoogleSignUp = async () => {
+
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white p-10 rounded shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            autoComplete="name"
+
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-green-500"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-green-500"
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="new-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-green-500"
+          />
+        </div>
+        <button onClick={handleSignUp} className="bg-green-500 text-white p-2 rounded-md mb-4 w-full">
+          Sign Up
+        </button>
+        <button
+          onClick={handleGoogleSignUp}
+          className="bg-red-500 text-white p-2 rounded-md w-full"
+        >
+          Sign Up with Google
+        </button>
+
+
+        <p className='mt-3'>Already a User ? <Link href={"./login"} className='font-bold'>Login</Link> </p>
+      </div>
+    </div>
+  );
 };
 
-export default Signup;
+export default SignUp;
